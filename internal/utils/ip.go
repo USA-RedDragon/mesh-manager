ede706b2b36c1ddc1671ef5eed7fdee09ac52532
Jacob McSwain <jacob@mcswain.dev>
2025-12-20 15:28:17 -0600

    wg: fix ipv6ll generation

--- internal/utils/ip.go
+++ internal/utils/ip.go
@@ -6,11 +6,40 @@ import (
 )
 
 // Generate a link-local address beginning with fe80 and ending with the 4 octets of the IPv4 address
+// The generation logic follows the upstream wireguard-tools implementation:
+// 1. Create a pseudo-MAC address: 00:00:IPv4
+// 2. Convert to EUI-64 IPv6 Link-Local address
 func GenerateIPv6LinkLocalAddress(ipv4 net.IP) (string, error) {
 	v4Bytes := ipv4.To4()
-	ipv6 := net.ParseIP(fmt.Sprintf("fe80::%02x%02x:%02x%02x", v4Bytes[0], v4Bytes[1], v4Bytes[2], v4Bytes[3]))
-	if ipv6 == nil {
-		return "", fmt.Errorf("failed to parse IPv6 address")
+	if v4Bytes == nil {
+		return "", fmt.Errorf("invalid IPv4 address")
 	}
+
+	// Construct the IPv6 address bytes
+	// Prefix: fe80::/64
+	ipv6Bytes := make([]byte, 16)
+	ipv6Bytes[0] = 0xfe
+	ipv6Bytes[1] = 0x80
+
+	// Interface ID derived from pseudo-MAC 00:00:v4[0]:v4[1]:v4[2]:v4[3]
+	// Modified EUI-64: Flip 7th bit of first byte of MAC, insert ff:fe in middle
+
+	// MAC[0] is 0x00. Modified EUI-64 flips the universal/local bit (bit 1, value 0x02).
+	ipv6Bytes[8] = 0x00 ^ 0x02
+	// MAC[1] is 0x00.
+	ipv6Bytes[9] = 0x00
+	// MAC[2] is v4Bytes[0]
+	ipv6Bytes[10] = v4Bytes[0]
+	// Insert 0xff 0xfe
+	ipv6Bytes[11] = 0xff
+	ipv6Bytes[12] = 0xfe
+	// MAC[3] is v4Bytes[1]
+	ipv6Bytes[13] = v4Bytes[1]
+	// MAC[4] is v4Bytes[2]
+	ipv6Bytes[14] = v4Bytes[2]
+	// MAC[5] is v4Bytes[3]
+	ipv6Bytes[15] = v4Bytes[3]
+
+	ipv6 := net.IP(ipv6Bytes)
 	return ipv6.String(), nil
 }
---
+++ internal/utils/ip_test.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"net"
+	"testing"
+)
+
+func TestGenerateIPv6LinkLocalAddress(t *testing.T) {
+	tests := []struct {
+		name     string
+		ipv4     string
+		expected string
+		wantErr  bool
+	}{
+		{
+			name:     "192.168.1.1",
+			ipv4:     "192.168.1.1",
+			expected: "fe80::200:c0ff:fea8:101",
+			wantErr:  false,
+		},
+		{
+			name:     "10.0.0.1",
+			ipv4:     "10.0.0.1",
+			expected: "fe80::200:aff:fe00:1",
+			wantErr:  false,
+		},
+		{
+			name:     "Invalid IP",
+			ipv4:     "256.0.0.1",
+			expected: "",
+			wantErr:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ipv4 := net.ParseIP(tt.ipv4)
+			// ParseIP returns nil for invalid IP, but we need to handle the case where we pass something that isn't an IP string if we want to test ParseIP failure, but here we are testing GenerateIPv6LinkLocalAddress which takes net.IP.
+			// If net.ParseIP returns nil, GenerateIPv6LinkLocalAddress should handle it (it calls To4() which returns nil on nil receiver? No, To4 on nil IP returns nil).
+			
+			// If ipv4 is nil (invalid parse), we pass it.
+			
+			got, err := GenerateIPv6LinkLocalAddress(ipv4)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GenerateIPv6LinkLocalAddress() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.expected {
+				t.Errorf("GenerateIPv6LinkLocalAddress() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
+}
